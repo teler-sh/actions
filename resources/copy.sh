0c4ae9d7282a2d96842dd1e9027d516342237ba2
Dwi Siswanto <git@dw1.io>
2024-03-26 22:17:22 +0700

    fix(resources): unbound ${1} variable
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- resources/copy.sh
+++ resources/copy.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 CACHE_PATH="$HOME/.cache/teler-waf/"
 TODAY_DATE="$(date +%d%m%Y)"
 
-echo "::debug::Copying '${CACHE_PATH}' into '${1}'"
+echo "::debug::Copying '${CACHE_PATH}' into '${TODAY_DATE}'"
 cp -a "${CACHE_PATH}" "/tmp/.teler-resources-${TODAY_DATE}"

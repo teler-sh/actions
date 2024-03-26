515f7df18a7a22b3a5758caa55b080699ae277a4
Dwi Siswanto <git@dw1.io>
2024-03-26 22:15:31 +0700

    fix(resources): incorrect `DB_FILE` dir path
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- resources/download.sh
+++ resources/download.sh
@@ -2,18 +2,19 @@
 
 set -euo pipefail
 
-CACHE_PATH="$HOME/.cache/teler-waf/"
+CACHE_PATH="$HOME/.cache/teler-waf"
 REPO_URL="https://github.com/kitabisa/teler-resources"
 DB_FILE="db.tar.zst"
+DB_OUTPUT="/tmp/${DB_FILE}"
 
 echo "::debug::Downloading '${DB_FILE}'"
-wget -q "${REPO_URL}/raw/master/db/${DB_FILE}" -q -O "/tmp/${DB_FILE}"
+wget -q "${REPO_URL}/raw/master/db/${DB_FILE}" -O "${DB_OUTPUT}"
 
 echo "::debug::Creating '${CACHE_PATH}' directory"
 mkdir -p "${CACHE_PATH}"
 
 echo "::debug::Extracting '${DB_FILE}' to '${CACHE_PATH}'"
-tar -I zstd -xf "${DB_FILE}" -C "${CACHE_PATH}"
+tar -I zstd -xf "${DB_OUTPUT}" -C "${CACHE_PATH}"
 
 # Run copy
 . "${GITHUB_ACTION_PATH}/copy.sh"
